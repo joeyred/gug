fa26f669b0f7b4c2fcc5b0b46aff3f448a49419e
Joey Hayes <bjoeyhayes@gmail.com>
2016-12-17 22:13:09 -0500

    Updated working code

--- index.js
+++ index.js
@@ -7,68 +7,71 @@ var yaml         = require('js-yaml');
 function parseYaml(file) {
   return yaml.safeLoad(fs.readFileSync(file));
 }
-// NOTE Gonna have to make this module handle the other settings for pug as well.
 module.exports = function(options) {
   var defaults = {
-    src: [
-      'src/pug/*.pug',
-      '!src/pug/layouts{,/**}',
-      '!src/pug/includes{,/**}'
-    ],
-    options: {
-      basedir: 'src',
-      pretty: true
-    },
-    data: {
-      configDirPath: './config/',
+    sourceDir: 'src',
+    pugDir: 'pug',
+    configDir: 'config',
+    configFiles: {
       siteConfig: 'site.yml',
       collectionsConfig: 'collections.yml'
     }
   };
   options = objectAssign(defaults, options);
 
-  function getConfigPaths(options) {
-    var output = {};
-    var pathToConfigDir = options.configDirPath;
-    var configFiles = {
-      siteConfig: options.siteConfig,
-      collectionsConfig: options.collectionsConfig
-    };
+  var basedir = options.sourceDir + '/' + options.pugDir;
+  var configDirPath = basedir + '/' + options.configDir;
 
-    for (var key in configFiles) {
-      if ({}.hasOwnProperty.call(values, key)) {
-        output[key] = pathToConfigDir + configFiles[key];
+  function isPugOption(option) {
+    for (var key in defaults) {
+      if ({}.hasOwnProperty.call(defaults, key)) {
+        if (key === 'basedir') {
+          return true;
+        }
+        if (key === option) {
+          return false;
+        }
       }
+    }
+    return true;
+  }
+  function getSrc() {
+    return [
+      basedir + '/*.pug',
+      '!' + basedir + '/layouts{,/**}',
+      '!' + basedir + '/includes{,/**}'
+    ];
+  }
+  function getSite() {
+    var siteConfigPath = './' + configDirPath + '/' + options.configFiles.siteConfig;
+    return parseYaml(siteConfigPath);
+  }
+  function getPug() {
+    var output = {};
+    var data = {
+      site: getSite()
+      // collections: getCollections();
+    };
+    // Grab any options passed outside of what gug uses.
+    for (var key in options) {
+      if ({}.hasOwnProperty.call(options, key)) {
+        if (isPugOption(key)) {
+          output[key] = options[key];
+        }
+      }
+    }
+    // If data wasn't passsed, then create the `data` object,
+    // Else, merge it with gug data.
+    if (output.data === undefined) {
+      output.data = data;
+    } else {
+      output.data = objectAssign(output.data, data);
     }
     return output;
   }
 
-  var configPaths = getConfigPaths(options.data);
-
-  function site() {
-    var fileToParse = fs.readFileSync(configPaths.siteConfig);
-    var parsedFile = yaml.safeLoad(fileToParse);
-    return parsedFile;
-  }
-  function collections() {
-    // the output of any collection should be collections.collection.foo.bar
-    // The directory name should match the defined collection name
-    var collectionsConfig = this.parseYaml(configPaths.collectionsConfig);
-    // Gets each collection and their settings.
-
-    // Iterate over each collection
-    // for ( var collection in collections )
-
-    // Check if collection is active.
-    // Store in an array.
-    // loop through those and grab the items in each collection.
-    // assign the data as an object in an array.
-    // itterate through the array via pug.
-
-    return registeredCollections;
-  }
   return {
-    site: site(),
-    collections: collections()
+    src: getSrc(),
+    options: getPug()
   };
-}
+};
